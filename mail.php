89dcdeea628fc5a7dc81f59e259d8b254bec03cb
AlexJelani <jelanialexander82@gmail.com>
2022-07-14 10:57:32 +0900

    edited php

--- mail.php
+++ mail.php
@@ -11,11 +11,7 @@ if(isset($_POST['email']) && $_POST['email'] != ''){
   $subject = $_POST['subject'];
   $message = $_POST['message'];
 
-<<<<<<< HEAD
   $to = "j-alexander@kaiyo.ac.jp";
-=======
-  $to = "jelanialexander82@gmail.com";
->>>>>>> ecc95779c58ac1836f3ea019fd01c2e1d763e111
   $body = "";
 
   $body .= "From: ".$name. "\r\n";
@@ -24,11 +20,7 @@ if(isset($_POST['email']) && $_POST['email'] != ''){
   $body .= "Message: ".$message. "\r\n";
 
 
-<<<<<<< HEAD
   mail($to, $subject, $body);
-=======
-  // mail($to, $subject, $body);
->>>>>>> ecc95779c58ac1836f3ea019fd01c2e1d763e111
 
   if($message_sent = true) {
 
@@ -71,54 +63,7 @@ if(isset($_POST['email']) && $_POST['email'] != ''){
   
 
 }
-
-
-
-
-// $mailheader = "From:".$name."<".$email.">\r\n";
-
-
-// $recipient = "jelanialexander82@gmail.com";
-
-// $success = mail($recipient, $subject, $message, $mailheader);
-// if(!$success) {
-//   $errorMessage = error_get_last()['message'];
-// }
-
-
-
-
-// echo '
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//   <link rel="stylesheet" href="src/styles.css">
-//   <script src="https://kit.fontawesome.com/b3efaa88b1.js" crossorigin="anonymous"></script>
-//   <link rel="preconnect" href="https://fonts.googleapis.com">
-//   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
-//   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&amp;display=swap" rel="stylesheet">
-//   <link rel="preconnect" href="https://fonts.googleapis.com">
-// <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-// <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-  
-//   <footer id="contact-me">
-//     <div class="container">
-//       <h1 class="contact">Thank you for contacting me. I will get back to you as soon as possible!</h1>
-//       <p class="back">Go back to the <a href="index.html">homepage</a>  </p>
-      
-//     </div>
-//     <p>Contact me</p>
-//     <p>jelanialexander82@gmail.com</p>
-//   </footer>
-
-// </body>
-// </html>
-
-
-// ';
-
-
 ?>
+
+
+
