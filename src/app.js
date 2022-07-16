96b49267881907ac9e0ca37048629f9b6ccc3df5
AlexJelani <jelanialexander82@gmail.com>
2022-07-16 16:57:16 +0900

    added langSwith JS to name attr

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
+var data = {
+  "english": {
+    "name": "Jelani Alexander"
+  },
+  "japanese": {
+    "name": "ジェラーニ　アレキサンダー"
+  }
+
+
+
+
+}
+
+
+const langEL = document.querySelector('.langWrap');
+const link = document.querySelectorAll('a');
+const nameEL = document.querySelector('.name');
+
+link.forEach(el => {
+  el.addEventListener('click', () => {
+    langEL.querySelector('.active').classList.remove('active');
+
+    el.classList.add('active');
+
+    const attr = el.getAttribute('language');
+
+    nameEL.textContent = data[attr].name;
+  });
+});
+
+
+
 const hamburgerButton = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
 
